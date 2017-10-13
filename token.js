4d1e4d143a8787e0a9b85596527e4d62082a1373
fengyinpeng <fengyp001@qianhai.com.cn>
2017-10-13 17:26:02 +0800

    token

---
+++ token.js
@@ -0,0 +1,69 @@
+
+/*
+appID
+wx6015b569d0e07ec9
+appsecret
+d4624c36b6795d1d99dcf0547af5443d
+
+https请求方式: GET
+https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
+*/
+
+var http = require('http')
+
+var TOKEN;
+
+
+function getAccessToken() {
+    if (TOKEN == null || TOKEN == undefined || TOKEN.len == 0) {
+        refreshAccessToken('wx6015b569d0e07ec9', 'd4624c36b6795d1d99dcf0547af5443d')
+        return null;
+    } else {
+        return TOKEN;
+    }
+}
+
+function refreshAccessToken(appID, appsecret) {
+    const url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appID + '&secret=' + appsecret;
+
+    http.get(url, (res) => {
+        const statusCode = res.statusCode;
+        const contentType = res.headers['content-type'];
+      
+        let error;
+        if (statusCode !== 200) {
+          error = new Error('Request Failed.\n' +
+                            `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error('Invalid content-type.\n' +
+                            `Expected application/json but received ${contentType}`);
+        }
+        if (error) {
+          console.log(error.message);
+          // consume response data to free up memory
+          res.resume();
+          return;
+        }
+      
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => rawData += chunk);
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            TOKEN = parsedData.access_token;
+
+            console.log(parsedData);
+          } catch (e) {
+            console.log(e.message);
+          }
+        });
+      }).on('error', (e) => {
+        console.log(`Got error: ${e.message}`);
+      });
+}
+
+module.exports = {
+    get: getAccessToken,
+    refresh: refreshAccessToken,
+};
