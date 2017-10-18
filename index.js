b3057e0057a55a927eaa93f03d62087b2537abd0
fengyinpeng <fengyp001@qianhai.com.cn>
2017-10-18 16:26:13 +0800

    msg2

--- index.js
+++ index.js
@@ -19,22 +19,24 @@ function sha1(str) {
 }
 
 function signature_handler(req, res, next) {
-  var q = req.query;
-  var token = TOKEN;
-  var signature = q.signature; //微信加密签名  
-  var nonce = q.nonce; //随机数  
-  var timestamp = q.timestamp; //时间戳  
-  var echostr = q.echostr; //随机字符串  
-  /* 
-      1）将token、timestamp、nonce三个参数进行字典序排序 
-      2）将三个参数字符串拼接成一个字符串进行sha1加密 
-      3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信 
-  */
-  var str = [token, timestamp, nonce].sort().join('');
-  var sha = sha1(str);
-  console.log('sha1 = ' + sha + ', p = ' + JSON.stringify(q));
+  console.log(JSON.stringify(req.query));
 
   if (req.method == 'GET') {
+    var q = req.query;
+    var token = TOKEN;
+    var signature = q.signature; //微信加密签名  
+    var nonce = q.nonce; //随机数  
+    var timestamp = q.timestamp; //时间戳  
+    var echostr = q.echostr; //随机字符串  
+    /* 
+        1）将token、timestamp、nonce三个参数进行字典序排序 
+        2）将三个参数字符串拼接成一个字符串进行sha1加密 
+        3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信 
+    */
+    var str = [token, timestamp, nonce].sort().join('');
+    var sha = sha1(str);
+    console.log('sha1 = ' + sha + ', p = ' + JSON.stringify(q));
+
     if (sha == signature) {
       res.send(echostr + '')
     } else {
@@ -45,14 +47,28 @@ function signature_handler(req, res, next) {
     var data = "";
     req.on('data', function(chunk){ data += chunk})
     req.on('end', function(){
-      var parseString = require('xml2js').parseString;
+      var xml2js = require('xml2js');
+      var parseString = xml2js.parseString;
+
       parseString(req.body, function (err, result) {
-          
+          if (err) {
+            res.send('success');
+          } else {
+              var tmp = result.FromUserName;
+              result.FromUserName = result.ToUserName;
+              result.ToUserName = tmp;
+
+              var builder = new xml2js.Builder({
+                rootName: 'test',
+                xmldec:  {'version': '1.0', 'encoding': 'UTF-8'},
+              });
+
+              var resMsg = builder.buildObject(result);
+              res.send(resMsg);
+          }
       });
       
       console.log(data);
-
-      res.send('success');
       next();
     })
   }
--- xmltest.js
+++ xmltest.js
@@ -1,6 +1,22 @@
-var parseString = require('xml2js').parseString;
+var xml2js = require('xml2js');
+var parseString = xml2js.parseString;
 var xml = "<root>Hello xml2js!</root>"
 parseString(xml, function (err, result) {
-    console.dir(err);
-    console.dir(result);
+    
+
+    if (err) {
+        console.log('failed');
+    } else {
+        console.dir(result);
+    }
+    
 });
+
+var obj = {name: "Super", Surname: "Man", age: 23};
+
+var builder = new xml2js.Builder({
+    rootName: 'test',
+    xmldec:  {'version': '1.0', 'encoding': 'UTF-8'},
+});
+var xml = builder.buildObject(obj);
+console.log(xml);
